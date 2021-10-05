658e8651678ea4ae4e87045ffea8ca272f883496
fuckl0l <90489436+fuckl0l@users.noreply.github.com>
2021-10-05 08:16:16 +0700

    Create commit.py

---
+++ commit.py
@@ -0,0 +1,12 @@
+import os
+
+ip = 100
+autoPush = y
+
+for i in range(ip):
+	os.system(f'git commit --allow-empty -m "Commit {i} of {ip}"')	
+
+print("Commited " + str(ip) + " times")
+
+if autoPush == "y":
+	os.system('git push')
