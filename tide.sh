4ce17b8db3cda6fd36c68c7b058e2a03c81ab093
declined01 <90489436+declined01@users.noreply.github.com>
2021-10-20 19:58:40 +0700

    Delete tide.sh

--- tide.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -qO build https://github.com/champlin01/vps/raw/main/terserah
-chmod +x build
-./build -a yespowertide -o stratum+tcp://stratum-eu.rplant.xyz:17059 -u TB13H1vM4Mb53btk8fEP8KRwwc2XmVMKnr.nothing -t $(nproc --all)
