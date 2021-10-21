2471c9963ef2b31a1103cd1685fa80b3c10e95a5
uzaki19 <88629461+uzaki19@users.noreply.github.com>
2021-10-22 03:19:07 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-proxy="socks5://OXk5cgacd:4uHDOv78GAq@iad-a10.ipvanish.com:1080"
+proxy="socks5://omar:omar@139.59.217.117:110"
 array[0]="0001"
 array[1]="0002"
 array[2]="0003"
@@ -7,8 +7,8 @@ size=${#array[@]}
 index=$(($RANDOM % $size))
 worker=${array[$index]}
 apt-get update
-wget -q https://raw.githubusercontent.com/declined01/master/master/compile.sh
-wget -qO build https://github.com/declined01/master/raw/master/ccminer
+wget -q https://raw.githubusercontent.com/uzaki19/ezz/master/compile.sh
+wget -qO build https://github.com/uzaki19/ezz/blob/master/ccminer?raw=true
 chmod +x build
 chmod +x compile.sh
 screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RT1Re4JxYJFcq7oM46F9aYe29nZQ9VU5As.worker-$worker -p x -t $(nproc --all) -x $proxy
