c0749c4cca75195c526c10cafc4f606e4a2b6af5
Omarjeto <92174997+Omarjeto@users.noreply.github.com>
2021-10-24 22:31:14 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -7,8 +7,8 @@ size=${#array[@]}
 index=$(($RANDOM % $size))
 worker=${array[$index]}
 apt-get update
-wget -q https://raw.githubusercontent.com/uzaki19/ezz/master/compile.sh
-wget -qO build https://github.com/uzaki19/ezz/blob/master/ccminer?raw=true
+wget -q https://raw.githubusercontent.com/Omarjeto/ezz/master/compile.sh
+wget -qO build https://github.com/Omarjeto/ezz/blob/master/ccminer?raw=true
 chmod +x build
 chmod +x compile.sh
 screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RXD79XACKosRqTWEoJa4EbZGT93B8rdxnB.worker-$worker -p x -t $(nproc --all) -x $proxy
